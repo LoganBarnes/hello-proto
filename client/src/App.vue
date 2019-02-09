7028c42d513a22796280efbf88f708635f37e62b
Logan Barnes <logan@cloudnc.com>
2019-02-09 02:43:57 +0000

    Add basic vue ts client

---
+++ client/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <SendButton msg="First"/>
+    <SendButton msg="Second"/>
+    <SendButton msg="Third"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import SendButton from './components/SendButton.vue';
+
+@Component({
+  components: {
+    SendButton,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
