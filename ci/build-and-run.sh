a216cba8e7ee675eda303befdf422463fa57d0c1
Logan Barnes <barneslt63@gmail.com>
2019-02-16 21:21:05 +0000

    Update build-and-run.sh

--- ci/build-and-run.sh
+++ ci/build-and-run.sh
@@ -15,7 +15,7 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 
 # node update
-curl -sL https://deb.nodesource.com/setup_10.x | -E bash -
+curl -sL https://deb.nodesource.com/setup_10.x | bash -
 
 apt update
 apt install -y lcov nodejs yarn
