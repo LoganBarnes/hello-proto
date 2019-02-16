e358a7c8c45c91be223344a8bf8d2e08540cf8e2
Logan Barnes <logan@cloudnc.com>
2019-02-16 15:40:29 +0000

    Remove sudo from ci script

--- ci/build-and-run.sh
+++ ci/build-and-run.sh
@@ -10,8 +10,8 @@ cmake --version
 apt update
 
 # yarn repo
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 
 apt update
 apt install -y lcov yarn
