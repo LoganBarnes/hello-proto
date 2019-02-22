7cfd487e0af7b40db00195e0c9405d724918a2bc
Logan Barnes <logan@cloudnc.com>
2019-02-22 20:22:10 +0000

    Add ci to master as well

---
+++ ci/build-and-run.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -e # fail script if any individual commands fail
+
+# set cmake path and test version
+export PATH=/deps/cmake/bin:$PATH
+cmake --version
+
+# install deps
+apt update
+
+# yarn repo
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+
+apt update
+apt install -y lcov yarn
+
+# build server
+pushd server
+function build_and_run {
+    cmake -E make_directory $1
+    cmake -E chdir $1 cmake -DCMAKE_BUILD_TYPE=$2 -DMCS_USE_DEV_FLAGS=ON ..
+    cmake -E chdir $1 cmake --build . --parallel
+    cmake -E chdir $1 cmake ./$3
+}
+
+build_and_run cmake-build-debug Debug hello_coverage
+build_and_run cmake-build-release Release hello_tests
+popd
+
+# build client
+pushd client
+yarn
+yarn generate
+yarn build
+popd
